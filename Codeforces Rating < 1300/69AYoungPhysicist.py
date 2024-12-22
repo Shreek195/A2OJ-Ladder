4bdfd00099197580b6c524f209e5b3e813b9fbf3
Shree Koshti <76033482+Shreek195@users.noreply.github.com>
2024-12-22 14:31:43 +0530

    Create 69AYoungPhysicist.py

---
+++ Codeforces Rating < 1300/69AYoungPhysicist.py
@@ -0,0 +1,14 @@
+def youngPhysicist():
+    n = int(input())
+    x = y = z = 0
+
+    for _ in range(n):
+        a, b, c = map(int, input().split())
+        x += a
+        y += b
+        z += c
+
+    print("YES" if x == 0 and y == 0 and z == 0 else "NO")
+
+if __name__ == "__main__":
+    youngPhysicist()
