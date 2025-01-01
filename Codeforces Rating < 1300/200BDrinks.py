c6b72558214560a55c645abc0265b1a0509733a8
Shree Koshti <76033482+Shreek195@users.noreply.github.com>
2025-01-01 16:01:53 +0530

    Update 200BDrinks.py

--- Codeforces Rating < 1300/200BDrinks.py
+++ Codeforces Rating < 1300/200BDrinks.py
@@ -1,7 +1,7 @@
 def drinks(n, nums):
-    return sum(nums) / n
+    print(sum(nums) / n)
 
 if __name__ == "__main__":
     n = int(input())
     nums = list(map(int, input().rstrip().split()))
-    print(drinks(n, nums))
+    drinks(n, nums)
